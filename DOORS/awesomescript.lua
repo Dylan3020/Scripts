ca1145ca8689de03b0ff7288d6e34c1e9f108420
Dylan3020 <144632565+Dylan3020@users.noreply.github.com>
2024-09-08 13:34:55 -0500

    Update awesomescript.lua

--- DOORS/awesomescript.lua
+++ DOORS/awesomescript.lua
@@ -592,7 +592,7 @@ Tab:Toggle("Waste Other Players Items","Repeatedly uses everyone else's items li
         for _,Player in pairs(Players:GetPlayers()) do
             local function WasteItem(Item)
                 if Item.Parent ~= Character and Item.Parent.Parent ~= LocalPlayer then
-                    if ((Item.Name == "Lighter" or Item.Name == "Flashlight") and Item:GetAttribute("Enabled") == false) or Item.Name == "BatteryPack" or "LaserPointer" or Item.Name == "Glowsticks" or Item.Name == "Vitamins" then
+                    if ((Item.Name == "Lighter" or Item.Name == "Flashlight") and Item:GetAttribute("Enabled") == false) or Item.Name == "BandagePack" or Item.Name == "Candle" or Item.Name == "BatteryPack" or "LaserPointer" or Item.Name == "Glowsticks" or Item.Name == "Vitamins" then
                         Item.Remote:FireServer()
                     end
                 end
