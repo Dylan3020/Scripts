fb31efd88c149207f6f7be79c84a00ad4b3216ba
Dylan3020 <144632565+Dylan3020@users.noreply.github.com>
2024-09-23 19:19:23 -0500

    Create TrollFaceSpawner.lua

---
+++ Entity Spawner/TrollFaceSpawner.lua
@@ -0,0 +1,54 @@
+print("trolling")
+local spawner = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors/Entity%20Spawner/V2/Source.lua"))()
+local entity = spawner.Create({
+	Entity = {
+		Name = "Troll Face",
+		Asset = "https://github.com/Dylan3020/Scripts/raw/refs/heads/main/Entity%20Spawner/Trollface.rbxm",
+		HeightOffset = 1
+	},
+	Lights = {
+		Flicker = {
+			Enabled = true,
+			Duration = 5
+		},
+		Shatter = true,
+		Repair = false
+	},
+	Earthquake = {
+		Enabled = true
+	},
+	CameraShake = {
+		Enabled = true,
+		Range = 100,
+		Values = {1.5, 20, 0.1, 1}
+	},
+	Movement = {
+		Speed = 120,
+		Delay = 10,
+		Reversed = false
+	},
+	Rebounding = {
+		Enabled = true,
+		Type = "Ambush",
+		Min = 5,
+		Max = 13,
+		Delay = 5
+	},
+	Damage = {
+		Enabled = false,
+		Range = 40,
+		Amount = 125
+	},
+	Crucifixion = {
+		Enabled = true,
+		Range = 40,
+		Resist = false,
+		Break = true
+	},
+	Death = {
+		Type = "Guiding",
+		Hints = {"noob got trolled", ":Troll:", "lol", "try again"},
+		Cause = "Troll Face"
+	}
+})
+entity:Run()
