7639cbf54f1f3bf6eb4c4cd68e95dcdd6cbb0305
Dylan3020 <144632565+Dylan3020@users.noreply.github.com>
2024-10-02 15:18:01 -0500

    Update mspaint.lua

--- DOORS/mspaint.lua
+++ DOORS/mspaint.lua
@@ -1,3 +1,13 @@
 print("mspaint loaded")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dylan3020/mspaint/refs/heads/main/places/doors/doors.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dylan3020/mspaint/refs/heads/main/main.lua"))()
+
+game.Lighting.Sky:Remove()
+game.Lighting.Bloom:Remove()
+game.Lighting.Caves:Remove()
+game.Lighting.ColorCorrection:Remove()
+game.Lighting.MainColorCorrection:Remove()
+game.Lighting.OxygenBlur:Remove()
+game.Lighting.OxygenCC:Remove()
+game.Lighting.Sanity:Remove()
+game.Lighting.XBoxColor:Remove()
