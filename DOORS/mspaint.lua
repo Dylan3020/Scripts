1799f31a83de7d6316dfc674dc8fc849de22630c
Dylan3020 <144632565+Dylan3020@users.noreply.github.com>
2024-10-01 21:08:11 -0500

    mspaint

--- DOORS/mspaint.lua
+++ DOORS/mspaint.lua
@@ -1,12 +1,3 @@
 print("mspaint loaded")
 
-loadstring(game:HttpGet("https://raw.githubusercontent.com/notpoiu/mspaint/main/main.lua"))()
-game.Lighting.Sky:Remove()
-game.Lighting.Bloom:Remove()
-game.Lighting.Caves:Remove()
-game.Lighting.ColorCorrection:Remove()
-game.Lighting.MainColorCorrection:Remove()
-game.Lighting.OxygenBlur:Remove()
-game.Lighting.OxygenCC:Remove()
-game.Lighting.Sanity:Remove()
-game.Lighting.XBoxColor:Remove()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Dylan3020/mspaint/refs/heads/main/places/doors/doors.lua"))()
