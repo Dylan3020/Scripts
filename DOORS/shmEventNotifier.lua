dd13ac14043d46330fce55646081b026f42bf6cf
Dylan3020 <144632565+Dylan3020@users.noreply.github.com>
2026-02-10 20:31:38 -0500

    sorry for that lol

--- DOORS/shmEventNotifier.lua
+++ DOORS/shmEventNotifier.lua
@@ -1,66 +1,17 @@
-function spawned(entity)
-game:GetService("StarterGui"):SetCore("SendNotification",{
-    Title = entity.." Has Spawned!", -- Required
-    Text = entity, -- Required!!
-    Icon = "rbxassetid://1234567890" -- Optional
-})
-end
-game.Workspace.ChildAdded:Connect(function(child)
+local plr = game.Players.LocalPlayer
+local char = plr.Character or plr.CharacterAdded:Wait()
+local hum = char:FindFirstChildOfClass("Humanoid") or char:WaitForChild("Humanoid")
+local entitynames = {"RushMoving","AmbushMoving", "A60", "A120", "Eyes", "Screech", "BackdoorRush", "BackdoorLookman", "Groundskeeper", "JeffTheKiller", "Rush", "Ambush"}
+ local addconnect
+        addconnect = workspace.ChildAdded:Connect(function(v)
+            if table.find(entitynames,v.Name) then
+                repeat task.wait() until plr:DistanceFromCharacter(v:GetPivot().Position) < 1000 or not v:IsDescendantOf(workspace)
+                
+                if v:IsDescendantOf(workspace) then
+        firesignal(game.ReplicatedStorage.EntityInfo.Caption.OnClientEvent, v.Name:gsub("Moving","").." Spawned!")
 
-if child.Name == "A60Moving" then
-spawned("A-60")
-elseif child.Name == "A120Moving" then
-spawned("A-120")
-elseif child.Name == "RushMoving" then
-spawned("Rush")
-elseif child.Name == "AmbushMoving" then
-spawned("Ambush")
 
-elseif child.Name == "TrollfaceMoving" then
-spawned("Trollface")
-elseif child.Name == "A60" then
-spawned("A-60")
-elseif child.Name == "A120" then
-spawned("A-120")
-elseif child.Name == "BackdoorRush" then
-spawned("Blitz")
-elseif child.Name == "MonumentEntity" then
-spawned("Monument")
-elseif child.Name == "Groundskeeper" then
-spawned("Groundskeeper")
-elseif child.Name == "Eyes" then
-spawned("Eyes")
-elseif child.Name == "BackdoorLookman" then
-spawned("Lookman")
-elseif child.Name == "Josh HutchersonMoving" then
-spawned("Josh Hutcherson")
-elseif child.Name == "werid entityMoving" then
-spawned("werid entity")
-elseif child.Name == "BlitzMoving" then
-spawned("Blitz")
-elseif child.Name == "Ambush_ModifierMoving" then
-spawned("Ambush")
-elseif child.Name == "ShadowA60Moving" then
-spawned("Shadow A-60")
-elseif child.Name == "ShadowA120Moving" then
-spawned("Shadow A-120")
-elseif child.Name == "AnglerMoving" then
-spawned("Angler")
-elseif child.Name == "FrogerMoving" then
-spawned("Froger")
-elseif child.Name == "eyeMoving" then
-spawned("eye")
-elseif child.Name == "scaryfaceMoving" then
-spawned("Scary Face")
-elseif child.Name == "CustomMoving" then
-spawned("Custom Entity")
-elseif child.Name == "Jeff The Killer" then
-spawned("Jeff The Killer")
-elseif child.Name == "PandemoniumMoving" then
-spawned("Pandemonium")
-elseif child.Name == "ContentMoving" then
-spawned("Content")
 
-end
-end)
-spawned("Entity Notifier 2.0")
+                end
+            end
+        end)
